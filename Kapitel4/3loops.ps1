ceb458e6432dcf8d40d6710df75f918019de36e9
Philip LearningIT <philip@learning-it.io>
2021-06-01 22:52:03 +0200

    updated Loops

--- Kapitel4/3loops.ps1
+++ Kapitel4/3loops.ps1
@@ -6,3 +6,33 @@ for ($i = 0; $i -lt $array.Count; $i++) {
 
 
 }
+
+# While Schleife
+Restart-Computer -ComputerName adserver01 -Force 
+Start-Sleep -Seconds 10
+do {
+    Start-Sleep -Seconds 2
+    Write-Host "Host Rebooting.."
+} until (Test-Connection -ComputerName adserver01 -Count 1 -Quiet)
+
+# Until Schleife
+Restart-Computer -ComputerName adserver01 -Force 
+Start-Sleep -Seconds 10
+do {
+    Start-Sleep -Seconds 2
+    Write-Host "Host Rebooting.."
+} while (!(Test-Connection -ComputerName adserver01 -Count 1 -Quiet))
+
+
+# ForEach Schleife
+$array = "Test1", "Test2", "Test3", "Test4"
+
+# For-Schleife (dient zum Vergleich)
+for ($i = 0; $i -lt $array.Count; $i++) {
+    $array[$i]
+}
+
+# ForEach:
+foreach ($one in $array){
+    Write-Host $one
+}
